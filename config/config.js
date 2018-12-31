f7dd794f3ce6355a23109c70f66e36f0f6b2d93c
JosueArronis <30699232+JosueArronis@users.noreply.github.com>
2018-12-30 18:40:34 -0600

    Google Sign in

--- config/config.js
+++ config/config.js
@@ -1,1 +1,4 @@
 module.exports.SEED = '@este-es@-un-seed-dificil';
+
+//Google
+module.exports.CLIENT_ID_GOOGLE = '407068943150-4bj9s4quu94o1ui751ocr8eb8hruc2i6.apps.googleusercontent.com';
--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,103 @@ var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 var SEED = require('../config/config').SEED;
+
+// Google
+var CLIENT_ID_GOOGLE = require('../config/config').CLIENT_ID_GOOGLE;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID_GOOGLE);
+
 var app = express();
+
+// Models
 var Usuario = require('../models/usuario');
+
+
+// =====================================================
+// Login Google
+// =====================================================
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID_GOOGLE,
+    });
+    const payload = ticket.getPayload();
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post('/google', async(req, res) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch(err => {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'Token no valido'
+            });
+        });
+
+    Usuario.findOne({ email: googleUser.email }, (err, userDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+        if (userDB) {
+            if (userDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe usar su atenticacion normal',
+                });
+            } else {
+                userDB.password = ':)';
+                var token = jwt.sign({ usuario: userDB }, SEED, { expiresIn: 14400 }); // 4 hours
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: userDB,
+                    token: token,
+                    id: userDB._id
+                });
+            }
+        } else {
+            // new user dont exist, need create it
+
+            var usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al buscar usuario',
+                        errors: err
+                    });
+                }
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 hours
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+            });
+        }
+
+    });
+});
 
 
 //=======================================================
